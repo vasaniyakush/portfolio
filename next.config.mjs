4720d1cfac5ba934800d83d930f5c39ceacd9bc1
Kush Vasaniya <vasaniyakush@gmail.com>
2025-06-05 13:14:48 +0530

    Update next.config.mjs
    Added redirect to meeting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+      return [
+        {
+          source: '/meet',
+          destination: 'https://meet.google.com/bqh-ridw-qpg', 
+          permanent: true, // 308 Permanent Redirect
+        },
+      ];
+    },
+};
 
 export default nextConfig;
